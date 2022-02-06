095948208fc7416a80256dead373476d3abf0d6e
saurabhyadav13 <90502564+saurabhyadav13@users.noreply.github.com>
2022-02-06 22:25:48 +0530

    Create Driver.java

---
+++ solutiion2/Driver.java
@@ -0,0 +1,58 @@
+
+package com.xyz.BST;
+
+
+class Node {
+    int value;
+    Node left, right;
+
+    Node(int item) {
+        value = item;
+        left = right = null;
+    }
+}
+class Driver {
+    public Node node;
+    public Node prevNode = null;
+    public Node headNode = null;
+
+    
+    public void createSkewedTree(Node root, int order) {
+        if (root == null) {
+            return;
+        }
+
+        if (order > 0) {
+            createSkewedTree(root.right, order);
+        } else {
+            createSkewedTree(root.left, order);
+        }
+        Node rightNode = root.right;
+        Node leftNode = root.left;
+
+        if (headNode == null) {
+            headNode = root;
+            root.left = null;
+            prevNode = root;
+        } else {
+            prevNode.right = root;
+            root.left = null;
+            prevNode = root;
+        }
+
+        if (order > 0) {
+            createSkewedTree(leftNode, order);
+        } else {
+            createSkewedTree(rightNode, order);
+        }
+    }
+
+    
+    public void traverseRightSkewed(Node root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.value + " -> ");
+        traverseRightSkewed(root.right);
+    }
+}
